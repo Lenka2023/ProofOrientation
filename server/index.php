a14837141b08620006246596b4d02e94b42b4240
uncleyura1976 <permmemo@gmail.com>
2015-04-04 04:40:27 +0300

    add code to server/index.php for random session ID counting byMersenne Twister tool

--- server/index.php
+++ server/index.php
@@ -1,4 +1,23 @@
 <?php
+// include library for Mersenne Twister tools
+require_once "mersenne_twister.php";
+
+use mersenne_twister\twister;
+
+#--------------------------------------------
+// initialize random function by currant time
+mt_srand(time());
+// initialize currant session ID
+$id = '';
+// create and initialize new object for session ID counting
+$twister = new twister(mt_rand());
+// create random session ID by 4 random hexadecimal connection
+for($i = 0; $i < 4; $i++)
+$id = $id.dechex($twister->int32());
+// setup session ID
+session_id($id);
+
+session_start();
 
 // change the following paths if necessary
 $yii='/opt/yii/framework/yii.php';
